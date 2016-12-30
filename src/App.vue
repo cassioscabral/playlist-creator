1874cc53bab3d0008ab95264c34746fa0dc79933
Cassio Cabral <cassioscabral@gmail.com>
2016-12-29 21:42:43 -0300

    remove Hello app, add sass

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,14 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <hello></hello>
+    <h1>Playlist Creator</h1>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
 
 export default {
   name: 'app',
   components: {
-    Hello
   }
 }
 </script>
