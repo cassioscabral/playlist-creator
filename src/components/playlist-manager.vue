fba6e18e8c0d7a68fe9c92635f703e23ff2e2245
Cassio Cabral <cassioscabral@gmail.com>
2017-01-16 04:06:53 -0300

    add initial test chart

--- src/components/playlist-manager.vue
+++ src/components/playlist-manager.vue
@@ -40,6 +40,7 @@
           {{playlistName}} ({{totalSongs}}) ({{msToTime(totalDurationPlaylist)}})
           <span class="clickable" v-if="previousPlaylist"><a @click="undo() && cleanOrderedBy()">undo ↺</a></span>
         </div>
+        <canvas v-show="playlist.length > 0" ref="canvas" id="myChart" width="400" height="100"></canvas>
       </div>
       <orderable-table
         :headers="tableHeaders"
@@ -134,6 +135,47 @@ export default {
       }
     },
     msToTime
+  },
+  mounted () {
+    const ctx = this.$refs.canvas
+    let myChart = new window.Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+          label: '# of Votes',
+          data: [12, 19, 3, 5, 2, 3],
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+          ],
+          borderColor: [
+            'rgba(255,99,132,1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+          ],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        responsive: true,
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
+            }
+          }]
+        }
+      }
+    })
+    console.log('myChart', myChart)
   },
   components: {
     SongList,
