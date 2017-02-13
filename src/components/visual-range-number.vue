dd0f4b9e2577ad70ea7bd631a4440e6cedf909be
Cassio Cabral <cassioscabral@gmail.com>
2017-02-13 20:39:23 -0300

    add initial visual range number component

---
+++ src/components/visual-range-number.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="visual-range-number">
+    {{min}} - {{value}} - {{max}}
+  </div>
+</template>
+
+<style>
+
+</style>
+
+<script>
+export default {
+  name: 'visual-range-number',
+  props: {
+    min: {
+      type: Number,
+      default () {
+        return 0
+      }
+    },
+    max: {
+      type: Number,
+      default () {
+        return 1
+      }
+    },
+    value: {
+      type: Number,
+      required: true
+    }
+  }
+}
+</script>
