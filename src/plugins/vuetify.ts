c32309d2cef491210976c69f9d5e8ceb932583a7
Cassio Cabral <cassio.soares-cabral@avantgarde.de>
2020-08-05 00:08:59 +0200

    Cleanup initial Vuetify

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,6 +1,29 @@
-import Vue from 'vue'
-import Vuetify from 'vuetify/lib'
+import Vue from "vue";
+import Vuetify from "vuetify/lib";
+Vue.use(Vuetify);
 
-Vue.use(Vuetify)
-
-export default new Vuetify({})
+const opts = {
+  theme: {
+    themes: {
+      light: {
+        primary: "#1DB954",
+        secondary: "#b3b3b3",
+        accent: "#8c9eff",
+        error: "#b71c1c",
+        info: "#2196F3",
+        success: "#4CAF50",
+        warning: "#FB8C00"
+      },
+      dark: {
+        primary: "#191414",
+        secondary: "#212121",
+        accent: "#8c9eff",
+        error: "#b71c1c",
+        info: "#2196F3",
+        success: "#4CAF50",
+        warning: "#FB8C00"
+      }
+    }
+  }
+};
+export default new Vuetify(opts);
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,12 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-  </div>
+  <div class="home"></div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
 
 export default Vue.extend({
   name: 'Home',
-  components: {
-    HelloWorld
-  }
+  components: {}
 })
 </script>
